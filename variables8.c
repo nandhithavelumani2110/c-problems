019314239ca9e4c847c2561f322e5ecf5da347de
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-09 14:44:40 +0530

    Add files via upload

---
+++ variables8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a=10,b=2,mul;
+    int c=5,d=10,multi;
+    mul=a*b;
+    multi=c*d;
+    printf("%d\n",mul);
+    printf("%d\n",multi);
+    return 0;
+}
