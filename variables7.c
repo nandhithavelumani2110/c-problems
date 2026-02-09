9e831636cc972f41fa45bb71a11e049b88963a05
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-09 14:40:10 +0530

    Add files via upload

---
+++ variables7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a=10,b=5,sub;
+    int c=20,d=10,subtract;
+    sub=a-b;
+    subtract=c-d;
+    printf("%d\n",sub);
+    printf("%d\n",subtract);
+    return 0;
+}
