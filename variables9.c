bf7d32d111f1a936707dd7db5824e0b5e3008f44
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-09 15:01:38 +0530

    Add files via upload

---
+++ variables9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,div,division;
+    a=8,b=2;
+    c=10,d=2;
+    div=a/b;
+    division=c/d;
+    printf("%d\n",div);
+    printf("%d\n",division);
+    return 0;
+}
