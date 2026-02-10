a40df4483c64b4d603c658d2e7c57aac921c7953
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 12:16:47 +0530

    Add files via upload

---
+++ rel3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=5,b=5;
+    printf("%d\n",a==b,b==a);
+    int x=3,y=4;
+    printf("%d\n",x==y,y==x);
+    int p=0,q=0;
+    printf("%d\n",p==q,q==p);
+    return 0;
+}
