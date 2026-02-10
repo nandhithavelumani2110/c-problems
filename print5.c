58b3938192c5df620b5b5736bbb7a23da12c768f
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 11:02:34 +0530

    Add files via upload

---
+++ print5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=2,b=3,c=5;
+    printf("%d\n",a+b+c);
+    int x=1,y=4,z=5;
+    printf("%d\n",x+y+z);
+    int p=3,q=3,t=4;
+    printf("%d\n",p+q+t);
+    return 0;
+}
