7c6bd674a50fb04213a6b02a06292ca1173b68cf
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 10:02:20 +0530

    Update data type9.c

--- data type9.c
+++ data type9.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int a=10.0,c=25.0;
+    int a=10,c=25;
     float b=a;
     float d=c;
     printf("%.1f\n",a,b);
     printf("%.1f\n",c,d);
     return 0;
+
 }
