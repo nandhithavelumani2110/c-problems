05734a81c2b1040844a5aae379544e79bd45e04b
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 12:19:39 +0530

    Add files via upload

---
+++ rel4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=5,b=3;
+    printf("%d\n",a!=b,b!=a);
+    int x=4,y=4;
+    printf("%d\n",x!=y,y!=x);
+    int p=9,q=1;
+    printf("%d\n",p!=q,q!=p);
+    return 0;
+}
