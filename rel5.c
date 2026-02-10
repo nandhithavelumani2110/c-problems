d0c0f8a5c4000e39f2ca5f4f8603975c09d6d042
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 13:06:41 +0530

    Add files via upload

---
+++ rel5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=5,b=5;
+    printf("%d %d\n",a>=b,a<=b);
+    int x=10,y=5;
+    printf("%d %d\n",x>=y,x<=y);
+    int p=3,q=7;
+    printf("%d %d\n",p>=q,p<=q);
+    return 0;
+}
