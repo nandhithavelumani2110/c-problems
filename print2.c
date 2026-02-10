f4524440378d01154f039e63936a72c710ffeb8a
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 10:26:49 +0530

    Add files via upload

---
+++ print2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=8,b=3;
+    printf("%d\n",a-b);
+    int c=10,d=5;
+    printf("%d\n",c-d);
+    int e=15,f=10;
+    printf("%d\n",e-f);
+    return 0;
+}
