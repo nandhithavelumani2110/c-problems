eb4861367a8598d4d4d13df683c4f4d272bd8ec9
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 12:14:29 +0530

    Add files via upload

---
+++ rel2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=5,b=10;
+    printf("%d\n",a<b,b<a);
+    int x=9,y=2;
+    printf("%d\n",x<y,y<x);
+    int p=4,q=4;
+    printf("%d\n",p<q,q<p);
+    return 0;
+}
