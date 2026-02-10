f3fa3b95fe4ed4bceae4bc3548a2c8486dc8e702
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 10:55:54 +0530

    Add files via upload

---
+++ print4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=9,b=2;
+    printf("quotient=%d remainder=%d\n",a/b,a%b);
+    int c=10,d=3;
+    printf("quotient=%d remainder=%d\n",c/d,c%d);
+    int e=20,f=4;
+    printf("quotient=%d remainder=%d\n",e/f,e%f);
+    return 0;
+}
