e4fc8184d62a732f61c76136e2cf60c7c0278dad
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 11:54:16 +0530

    Add files via upload

---
+++ increment3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a=10;
+    int b=--a;
+    printf("%d\n",b);
+    int x=1;
+    int z=--x;
+    printf("%d\n",z);
+    int p=5;
+    int f=--p;
+    printf("%d\n",f);
+    return 0;
+}
