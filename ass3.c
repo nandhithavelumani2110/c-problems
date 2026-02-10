aab86b9aa1dd3224078afde66797429779622194
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-10 13:18:30 +0530

    Add files via upload

---
+++ ass3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=10,b=5;
+    printf("%d\n",a-b);
+    int x=20,y=10;
+    printf("%d\n",x-y);
+    int p=5,q=5;
+    printf("%d\n",p-q);
+    return 0;
+}
