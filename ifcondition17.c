712a599bb357a879395d000d37162c2a24d3aafc
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-11 15:30:33 +0530

    Add files via upload

---
+++ ifcondition17.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a=15;
+    if(a>=10&&a<=20)
+    {
+        printf("the number is within range");
+    }
+    else 
+    {
+        printf("not in range ");
+    }
+    return 0;
+}
