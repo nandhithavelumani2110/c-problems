48acd5c79f05da03a5029acb0135a653293f5ec1
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-11 14:37:47 +0530

    Add files via upload

---
+++ ifcondition11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a=10,b=25,c=15;
+    if(a>b){
+        printf("%d is largest",a);
+    }
+    else if(b>c){
+        printf("%d is largest",b);
+    }
+    else{
+        printf("%d is largest",c);
+    }
+    return 0;
+}
