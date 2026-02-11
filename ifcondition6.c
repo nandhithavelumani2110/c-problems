18aa04d1ac2ebf7cf87c50fde2319d39e473c64e
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-11 11:09:55 +0530

    Add files via upload

---
+++ ifcondition6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char num='a';
+    if(num=='a'||num=='e'||num=='i'||num=='o'||num=='u')
+    {
+        printf("Vowel");
+    }
+    else{
+        printf("Constant");
+    }
+    return 0;
+}
