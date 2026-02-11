9e596962592235bfdf6b9d47f64c4b80b6a4cbe4
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-11 13:43:10 +0530

    Add files via upload

---
+++ ifcondition8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a=21;
+    if(a%3==0&&a%7==0)
+    {
+        printf("multiple of both 3 and 7 ");
+    }
+    else{
+        printf("not multiple");
+    }
+
+    return 0;
+}
