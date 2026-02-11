a794f2d78de72c1bf45747bc70ce06b214102989
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-11 14:19:57 +0530

    Add files via upload

---
+++ ifcondition12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a=82;
+    if(a>=90){
+        printf("grade A");
+    }
+    else if(a>=75){
+        printf("grade B");
+    }
+    else{
+        printf("grade c");
+        
+    }
+    return 0;
+}
