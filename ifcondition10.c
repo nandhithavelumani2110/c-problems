43a7820d86acbcb327e2d59771f0dbc79d628abe
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-11 14:33:50 +0530

    Add files via upload

---
+++ ifcondition10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a=345;
+    if(a>=100&&a<=999){
+        printf("three digit number");
+    }
+    else{
+        printf("not a three digit");
+    }
+    return 0;
+}
