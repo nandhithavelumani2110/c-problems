c062fda6d657a14aca74d646116e7ec549ad50fa
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-11 15:35:52 +0530

    Add files via upload

---
+++ ifcondition19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char ch=7;
+    if(ch>='0'&&ch<=9)
+    {
+        printf("digit");
+    }
+    else 
+    {
+        printf("digit ");
+    }
+    return 0;
+}
