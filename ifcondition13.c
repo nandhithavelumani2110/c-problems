1a28576c306694a7a20b0c569a06ca972fc49e3a
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-11 14:48:08 +0530

    Add files via upload

---
+++ ifcondition13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a=121;
+    if(a%11==0){
+        printf("divisble by 11");
+    }
+    else{
+        printf("not divisible by 11");
+    }
+    return 0;
+}
