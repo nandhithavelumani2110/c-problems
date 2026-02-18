168590f72f7d5cba00489c36934bd7ac0b1c0430
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-18 17:51:08 +0530

    Add files via upload

---
+++ for46.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int capacity, n, items;
+    int overflowDays = 0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&items);
+        capacity=capacity - items;
+        if(capacity<0)
+            overflowDays++;
+    }
+    printf("Remaining Capacity: %d\n", capacity);
+    printf("Overflow Days: %d", overflowDays);
+    return 0;
+}
