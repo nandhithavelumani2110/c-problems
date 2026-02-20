29fed130583cc9e7e67bb8fb4689a623cfe4006b
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-02-20 13:38:16 +0530

    Add files via upload

---
+++ while1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d",&N);
+    int i = 0;
+    int noise;
+    int violations=0;
+    int currentStreak=0;
+    int longestStreak=0;
+    while(i<N) {
+        scanf("%d",&noise);
+        if(noise>70) {
+            violations++;
+            currentStreak++;
+            if(currentStreak>longestStreak) {
+                longestStreak=currentStreak;
+            }
+        } else {
+            currentStreak=0;
+        }
+        i++;
+    }
+    printf("Noise Violations: %d\n",violations);
+    printf("Longest Violation Streak: %d",longestStreak);
+    return 0;
+}
