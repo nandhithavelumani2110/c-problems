36703a9c01e4aabbab823542418bad5b34fda96d
nandhithavelumani2110 <nandhithavelumani2110@gmail.com>
2026-03-17 13:39:20 +0530

    Add files via upload

---
+++ pointer3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int count=0;
+    fgets(str,sizeof(str),stdin);
+    char *ptr=str;
+    while(*ptr!='\0') {
+        if(*ptr=='a'||*ptr=='e'||*ptr=='i'||*ptr=='o'||*ptr=='u'||
+           *ptr=='A'||*ptr=='E'||*ptr=='I'||*ptr=='O'||*ptr=='U') {
+            count++;
+        }
+        ptr++;
+    }
+    printf("%d", count);
+    return 0;
+}
